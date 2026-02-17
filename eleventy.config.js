4e8cef434ab827fe59ff064a6941483b5a0ef3dd
Ron Jackson <ronjacksondev@outlook.com>
2026-02-17 11:06:13 -0600

    Adds base plugin and pathprefix to fix links

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,17 @@
-import { InputPathToUrlTransformPlugin } from "@11ty/eleventy";
+import { 
+  InputPathToUrlTransformPlugin,
+  EleventyHtmlBasePlugin,
+ } from "@11ty/eleventy";
 
 export default function(eleventyConfig) {
+  eleventyConfig.addPlugin (EleventyHtmlBasePlugin, {
+    baseHref: true ? "/full-stack-workflows/" : "/",
+  });
+
   eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
 
   return {
+    pathPrefix: "/full-stack-workflows/",
     dir: {
       input: ".",
       output: "_site"

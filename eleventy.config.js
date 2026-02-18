cda2b8c710314c3464d8a17a3b1ede7972fd4735
Ron Jackson <ronjacksondev@outlook.com>
2026-02-18 11:58:22 -0600

    Adds sytnax highlighting

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,8 @@ import {
   InputPathToUrlTransformPlugin,
   EleventyHtmlBasePlugin,
  } from "@11ty/eleventy";
+ import syntaxHighlight from '@11ty/eleventy-plugin-syntaxhighlight';
+
 
 export default function(eleventyConfig) {
   eleventyConfig.addGlobalData("layout", "base.html");
@@ -11,6 +13,8 @@ export default function(eleventyConfig) {
   });
 
   eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
+
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
     pathPrefix: "/full-stack-workflows/",

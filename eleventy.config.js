96f6cfa413661e9a5b469f24aed91f7aa59b0ec0
Ron Jackson <ronjacksondev@outlook.com>
2026-06-18 10:57:42 -0500

    Adjusts prism configuration

--- eleventy.config.js
+++ eleventy.config.js
@@ -17,9 +17,17 @@ export default async function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({
     "node_modules/prismjs/prism.js": "public/js/prism.js",
+    "node_modules/prismjs/themes/prism-tomorrow.css": "public/css/prism-tomorrow.css",
+    "node_modules/prismjs/plugins/autoloader/prism-autoloader.min.js": "public/js/prism-autoloader.min.js",
+    "node_modules/prismjs/components/prism-bash.min.js": "public/js/components/prism-bash.min.js",
+
     "node_modules/prismjs/plugins/toolbar/prism-toolbar.js": "public/js/prism-toolbar.js",
     "node_modules/prismjs/plugins/toolbar/prism-toolbar.css": "public/css/prism-toolbar.css",
-    "node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.js": "public/js/prism-copy-to-clipboard.js"
+    "node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.js": "public/js/prism-copy-to-clipboard.js",
+
+    "node_modules/prismjs/components/prism-diff.js": "public/js/prism-diff.js",
+    "node_modules/prismjs/plugins/diff-highlight/prism-diff-highlight.css": "public/css/prism-diff-highlight.css",
+    "node_modules/prismjs/plugins/diff-highlight/prism-diff-highlight.js": "public/js/prism-diff-highlight.js"
   });
 
   eleventyConfig.addPlugin (EleventyHtmlBasePlugin, {
